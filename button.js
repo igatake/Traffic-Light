c3b2c9556a26592fa96759f2abc8d08f9d58644e
Taketo Igai <t.igai.716@gmail.com>
2018-08-08 12:39:09 +0900

    Syntax error fixed

--- button.js
+++ button.js
@@ -1,21 +1,20 @@
-
 //ボタンが連打されることによる挙動の崩れを防ぐための変数を規定
 let isBlue = true;
 
 $('#btn').on('click', function(){
-  if (isBlue === false) 
+  if (isBlue === false) {
 //もしこれがfalseだった場合,
 //すでに信号は変化しているので,
 //ボタンを押しても作用しないようにする
     return;
   }
+
   blueToYellow();
 //setTimeoutで変化の順序を設定
 //赤=>青は8000-3000で５秒間で青に戻る設定
   setTimeout(YellowToRed, 3000);
   setTimeout(RedtoBlue, 8000);
 });
-
 
 //chg__opcの付け外しによりopactyを
 //1<=>0.2に変更することで点灯を表現。
