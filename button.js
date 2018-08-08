3285d1b962cf8133e46557edbcf034c9b0326611
Taketo Igai <t.igai.716@gmail.com>
2018-08-08 17:29:34 +0900

    refs #14 コメントの修正

--- button.js
+++ button.js
@@ -5,13 +5,11 @@ let pedColors = ['blue', 'red', 'blue']
 
 $('#btn').on('click', function(){
   if (isBlue === false) {
-//もしこれがfalseだった場合,
-//すでに信号は変化しているので,
+//もしこれがfalseだった場合すでに信号は変化しているので,
 //ボタンを押しても作用しないようにする
     return;
   }
   //setTimeoutで変化の順序を設定
-
   colorChanger(0);
   setTimeout('colorChanger(1)', 3000);
   setTimeout('pedColorChanger(1)', 4000);
@@ -20,8 +18,8 @@ $('#btn').on('click', function(){
   setTimeout('colorChanger(2)', 10000);
 });
 
-//chg__opcの付け外しによりopactyを
-//1<=>0.2に変更することで点灯を表現。
+//chg__opcの付け外しによりopactyを1<=>0.2に変更することで点灯を表現。
+//n=0で青=>黄, n=1で黄=>赤, n=2で赤=>青
 function colorChanger(n){
   let color1 = colors[n];
   let color2 = colors[n + 1];
@@ -41,10 +39,10 @@ function colorChanger(n){
 }
 
 //歩行者信号の色の遷移
+//n=0で青=>赤, n=1で赤=>青
 function pedColorChanger(n){
   let pedColor1 = pedColors[n];
   let pedColor2 = pedColors[n + 1];
-
   $('#ped__' + pedColor1).removeClass('chg__opc');
   $('#ped__' + pedColor2).addClass('chg__opc');
   if (n == 1) {
@@ -53,13 +51,13 @@ function pedColorChanger(n){
     .css('fontSize', '16px');
   }
 }
+
 //歩行者信号の点滅
 function flashingA(){
   for (let i = 0; i < 3; i++){
   setTimeout('pedColorChanger(0)', 7800 + i * 600);
   }
 }
-
 function flashingB(){
   for (let i = 0; i < 2; i++){
   setTimeout('pedColorChanger(1)', 8100 + i * 600);
